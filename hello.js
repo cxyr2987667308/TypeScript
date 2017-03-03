477c3ae73fbf635014b6f6744b6fd0c3ab2c695d
flj <lj.fang@zuolin.com>
2017-03-03 15:52:26 +0800

    className

--- hello.js
+++ hello.js
@@ -60,21 +60,21 @@ var __extends = (this && this.__extends) || (function () {
 /** 类 */
 // TypeScript支持继承了可选的类型批注支持的ECMAScript6的类
 // public 和 private访问修饰符，public成员可以在任何地方访问，private成员只允许在类中访问
-// class Shape{
-//     area: number;
-//     private color: string;
-//     constructor(public name: string, width: number, height: number){
-//         this.area = width * height;
-//         this.color = "pink";
-//     }
-//     shoutout(){
-//         return "I'm " + this.color + " " + this.name + " with an area of " + this.area + " cm squared.";
-//     }
-// }
-// var square = new Shape("square", 30, 30);
-// console.log(square.shoutout());
-// console.log('Area of Shape: ' + square.area);
-// console.log('Name of Shape: ' + square.name);
+var Shape = (function () {
+    function Shape(name, width, height) {
+        this.name = name;
+        this.area = width * height;
+        this.color = "pink";
+    }
+    Shape.prototype.shoutout = function () {
+        return "I'm " + this.color + " " + this.name + " with an area of " + this.area + " cm squared.";
+    };
+    return Shape;
+}());
+var square = new Shape("square", 30, 30);
+console.log(square.shoutout());
+console.log('Area of Shape: ' + square.area);
+console.log('Name of Shape: ' + square.name);
 // console.log('Color of Shape: ' + square.color);
 // console.log('Width of Shape: ' + square.width);
 // console.log('Height of Shape: ' + square.height);
